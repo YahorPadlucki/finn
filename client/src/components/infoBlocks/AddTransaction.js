a5200206d2f0c75049ceafb1c61744cb32c54bf6
Yahor Padlucki <yahor.padlucki@skywindgroup.com>
2020-01-22 08:48:24 +0300

    working on data submit form

--- client/src/components/infoBlocks/AddTransaction.js
+++ client/src/components/infoBlocks/AddTransaction.js
@@ -2,22 +2,25 @@ import React, {useEffect, useState} from 'react';
 
 const AddTransaction = () => {
     const [amount, setAmount] = useState('');
+    const [account, setAccount] = useState('');
+    const [category, setCategory] = useState('');
+
     let nameInput;
 
-    const onSubmit = (e) => {
-        e.preventDefault();
-
-        // this.setState({term: e.target.value})
-        // props.onSubmit(this.state.term)
-        console.log( e.target.value)
+    const onSaveClicked = (e) => {
+        console.log(amount)
+        console.log(category)
+        console.log(account)
     };
+
 
     useEffect(() => {
 // component did mount
+        console.log("effect")
         nameInput.focus();
     }, []);
 
-    const renderInputForm = function () {
+    const renderInputField = function () {
         return (
             <div className="field">
                 <label>Add Transaction</label>
@@ -32,20 +35,22 @@ const AddTransaction = () => {
     };
     const renderCategory = function () {
         return (
-            <select className="ui search dropdown">
+            <select className="ui search dropdown"
+                    onChange={e => setCategory(e.target.value)}>
                 <option value="">Category</option>
-                <option value="AL">Food</option>
-                <option value="AK">Flat</option>
+                <option value="Food">Food</option>
+                <option value="Flat">Flat</option>
             </select>
         );
     };
 
     const renderAccount = function () {
         return (
-            <select className="ui search dropdown">
+            <select className="ui search dropdown"
+                    onChange={e => setAccount(e.target.value)}>
                 <option value="">Account</option>
-                <option value="AL">Card</option>
-                <option value="AK">Cash</option>
+                <option value="Card">Card</option>
+                <option value="Cash">Cash</option>
             </select>
         );
     };
@@ -53,15 +58,15 @@ const AddTransaction = () => {
         <div className="ui container"
              style={{border: '1px solid rgba(34,36,38,.15)'}}>
             <div className="ui segment" style={{textAlign: "center"}}>
-                <form className="ui form" onSubmit={() => onSubmit()}>
-                    {renderInputForm()}
+                <form className="ui form" onSubmit={e => e.preventDefault()}>
+                    {renderInputField()}
                     {renderCategory()}
                     {renderAccount()}
-                    <input type="date" id="start" name="trip-start"
-                           value="2018-07-22">
-                    </input>
+                    {/*<input type="date" id="start" name="trip-start"*/}
+                    {/*value="2018-07-22">*/}
+                    {/*</input>*/}
                 </form>
-                <button className="big ui primary button ">Save</button>
+                <button className="big ui primary button " onClick={onSaveClicked}>Save</button>
             </div>
         </div>
     )
