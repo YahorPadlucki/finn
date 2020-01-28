4fc80b01b49f95fa1b238494e028afc28ab92268
Yahor Padlucki <yahor.padlucki@skywindgroup.com>
2020-01-28 08:52:58 +0300

    fetching data for accounts

--- client/src/components/infoBlocks/AddTransaction.js
+++ client/src/components/infoBlocks/AddTransaction.js
@@ -4,10 +4,11 @@ const AddTransaction = () => {
 
     //TODO: from server
     const categoryTypes = ["Food", "Flat"];
-    const accountTypes = ["Card", "Cash"];
+
+    const [accountTypes, setAccountTypes] = useState([]);
 
     const [amount, setAmount] = useState('');
-    const [account, setAccount] = useState(accountTypes[0]);
+    const [account, setAccount] = useState('');
     const [category, setCategory] = useState(categoryTypes[0]);
     const [date, setDate] = useState('');
     const [isInputValid, setIsInputValid] = useState(true);
@@ -36,9 +37,19 @@ const AddTransaction = () => {
 
     useEffect(() => {
         // component did mount
-        console.log("effect")
+        console.log("did mount")
 
-       fetch("http://localhost:3009/accounts").then((a)=>console.log(a.json()))
+        const fetchData = async () => {
+            const response = await fetch("http://localhost:3009/accounts")
+            const result = await response.json();
+            //
+
+            const accounts = result.map(el => el.name);
+            setAccountTypes(accounts);
+            setAccount(accounts[1])
+
+        };
+        fetchData();
         nameInput.focus();
         setCurrentDate();
 
@@ -48,6 +59,7 @@ const AddTransaction = () => {
         const date = new Date();
         const currentDate = date.getFullYear().toString() + '-' + (date.getMonth() + 1).toString().padStart(2, 0) +
             '-' + date.getDate().toString().padStart(2, 0);
+
         setDate(currentDate);
     };
 
@@ -67,13 +79,13 @@ const AddTransaction = () => {
         );
     };
 
-    const renderCategory = () => renderOptionsList(categoryTypes, setCategory);
+    const renderCategory = () => renderOptionsList(categoryTypes, setCategory, category);
 
-    const renderAccount = () => renderOptionsList(accountTypes, setAccount);
+    const renderAccount = () => renderOptionsList(accountTypes, setAccount, account);
 
-    const renderOptionsList = function (optionsArray, setStateFunction) {
+    const renderOptionsList = function (optionsArray, setStateFunction, selectedElement) {
         return (
-            <select className="ui search dropdown"
+            <select className="ui search dropdown" value={selectedElement}
                     onChange={e => setStateFunction(e.target.value)}>
                 {optionsArray.map((option, i) => <option key={i} value={option}>{option}</option>)}
             </select>
