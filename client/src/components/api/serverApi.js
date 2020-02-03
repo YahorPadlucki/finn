135ad47a36fa55b6bf9019fa3999c196edf34652
Yahor Padlucki <yahor.padlucki@skywindgroup.com>
2020-02-03 08:51:50 +0300

    json-server for multiple files

--- client/src/components/api/serverApi.js
+++ client/src/components/api/serverApi.js
@@ -1,5 +1,5 @@
 const fetchData = async (dataUrl) => {
-    const response = await fetch(`http://localhost:3009/${dataUrl}`);
+    const response = await fetch(`http://localhost:3002/${dataUrl}`);
     return await response.json();
 };
 
