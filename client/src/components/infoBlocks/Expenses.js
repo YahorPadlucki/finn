bc6616ed15e8ebb7e05824e22dcdaed52f9f8bf3
Yahor Padlucki <yahor.padlucki@skywindgroup.com>
2020-04-23 16:48:52 +0300

    calculate total expenses

--- client/src/components/infoBlocks/Expenses.js
+++ client/src/components/infoBlocks/Expenses.js
@@ -15,16 +15,26 @@ const Expenses = () => {
     } = useContext(AppContext);
 
     const [selectedDate, setSelectedDate] = useState({month: 1, year: 2020});
+    const [total, setTotal] = useState(0);
 
 
     useEffect(() => {
         fetchHistoryTransactions(selectedDate.year, selectedDate.month);
 
     }, [selectedDate]);
+
+    useEffect(() => {
+
+        if (historyTransactions.length) {
+            const total = historyTransactions.reduce((total, transaction) => total + transaction.total, 0);
+            setTotal(total);
+        }
+
+    }, [historyTransactions]);
     return (
         <div className="ui container"
              style={{border: '1px solid rgba(34,36,38,.15)'}}>
-            <div>Expenses:</div>
+            <div>Expenses: {total}</div>
             <DateSelector
                 onSelectedDateChanged={fetchHistoryTransactions}/>
         </div>
