c45d2e1ca5b184bf8927bc02e4f9ec08ec64485e
Yahor Padlucki <yahor.padlucki@skywindgroup.com>
2020-09-23 08:39:47 +0300

    Updates

---
+++ precache-manifest.dbe2325d6844ee80f40277c8b73e3165.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c3bc577eba3d3d0889fb6db892022d6",
+    "url": "/finn/index.html"
+  },
+  {
+    "revision": "a99428bc3d998ae50d83",
+    "url": "/finn/static/css/2.ecffae5e.chunk.css"
+  },
+  {
+    "revision": "56dc60744f28ec54b2fd",
+    "url": "/finn/static/css/main.347dd47e.chunk.css"
+  },
+  {
+    "revision": "a99428bc3d998ae50d83",
+    "url": "/finn/static/js/2.f850c8d5.chunk.js"
+  },
+  {
+    "revision": "5a719f1d9e6000be1ddec6da8b7044c6",
+    "url": "/finn/static/js/2.f850c8d5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "56dc60744f28ec54b2fd",
+    "url": "/finn/static/js/main.343c547e.chunk.js"
+  },
+  {
+    "revision": "3d46f4ec7fdf6e1ef994",
+    "url": "/finn/static/js/runtime-main.2ae2219e.js"
+  }
+]);
